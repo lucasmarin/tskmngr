b6e51f951e44d7d57fa653dc3fedad5959cd65d6
root <root@CentOS6.cloudtecla>
2012-10-09 01:23:56 -0400

    my First Commit

---
+++ src/Mandic/Bundle/TaskManagerBundle/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mandic\Bundle\TaskManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+}
---
+++ src/Mandic/Bundle/TaskManagerBundle/MandicTaskManagerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mandic\Bundle\TaskManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MandicTaskManagerBundle extends Bundle
+{
+}
